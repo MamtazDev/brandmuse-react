f97f936b71ad30158b93504b267b229766a2261f
tonni-akter-dev <tanniakhter123@gmail.com>
2024-09-13 12:44:29 +0600

    SDF

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { RouterProvider } from 'react-router-dom'
-import './App.css'
-import { router } from './Routes/Routes'
+import { RouterProvider } from 'react-router-dom';
+import './App.css';
+import { router } from './Routes/Routes';
 import { useEffect, useState } from 'react';
 
 function App() {
@@ -10,23 +10,18 @@ function App() {
     const handleLoad = () => {
       setIsLoading(false);
     };
-
     window.addEventListener('load', handleLoad);
+
     return () => window.removeEventListener('load', handleLoad);
   }, []);
 
   return (
     <>
-      {isLoading ? (
-        <div className="loader-wrapper">
-          <div className="loader"></div>
-        </div>
-      ) : (
+   
         <RouterProvider router={router} />
-      )}
 
     </>
-  )
+  );
 }
 
-export default App
+export default App;
